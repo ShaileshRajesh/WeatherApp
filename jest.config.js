b267a4ce4537933e64a232c8785aa9727dce9abc
Shailesh Rajesh M <156943530+ShaileshRajesh@users.noreply.github.com>
2024-11-20 23:23:34 +0530

    added jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|expo|expo-constants)/)',
+  ],
+  moduleNameMapper: {
+    'require\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
